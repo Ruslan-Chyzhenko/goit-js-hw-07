3bad384a90a5823afaef2eb04fcd272517f8ba78
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-01-05 16:31:07 +0100

    003

--- js/task-6.js
+++ js/task-6.js
@@ -1,15 +1,44 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-// Використовуй document.querySelector для отримання посилань на елементи <input>, <button data-create>, <button data-destroy>, і <div id="boxes">.
-// Homework:
 const inputElement = document.querySelector('input');
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
 const boxesContainer = document.querySelector('#boxes');
 
 createButton.addEventListener('click', createBoxes);
-destroyButtonButton.addEventListener('click', destroyBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = inputElement.value;
+
+
+if (amount < 1 || amount > 100) {
+  alert('Please enter a number between 1 and 100.');
+  return;
+}
+
+boxesContainer.innerHTML = '';
+
+for (let i = 0; i < amount; i++) {
+  const box = document.createElement('div');
+  const size = 30 + i * 10;
+  const color = getRandomHexColor();
+
+  box.style.width = '${ size } px';
+  box.style.height = '${ size } px';
+  box.style.backgroundColor = color;
+
+  boxesContainer.appendChild(box);
+}
+
+inputElement.value = '';
+
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
